c8b61fc2d0e975a068aa3038101f9a50623ff815
wilmer sneider <wil788ramirez@hotmail.com>
2024-08-03 23:42:42 -0500

    create del servidor y conexion bd

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 
 import express from 'express'
+import dotenv from 'dotenv'
+import conectarDB from './config/db.js';
 
 const app = express();
+//llamos dotenv para leer variables de entorno
+dotenv.config()
+
+//llamamos la funcion de la bd
+conectarDB()
+
+
+app.use("/", (req, res) => {
+    res.send("hola express nuevamente con wilmer")
+})
+
+
+
+
+const PORT = process.env.PORT || 4000
 
 app.listen(4000, () => {
-    console.log('servidor funcionando prt 4000')
+    console.log(`servidor funcionando prt ${PORT}`)
 })
